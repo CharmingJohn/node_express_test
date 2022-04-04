062dd5bf2a31e2ce1bcc66ab7d8460437c95c24b
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-04-04 23:49:57 +0900

    add author.js author model

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+
+//Set up mongoose connection
+var mongoose = requrie('mongoose');
+var mongoDB = require('');
+mongoose.connect(mongoDB, { userNewUrlParser: true, useUnfiiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
